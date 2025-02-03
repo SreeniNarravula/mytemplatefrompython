f0a7f0936c252ce84a24329f2e0cac778910a49f
SreeniNarravula <sreenivasulu.narravula@takeda.com>
2025-02-03 11:16:14 -0500

    Initialize dependabot/pip/mypy-gte-1.5-and-lt-1.15

---
+++ project_name/_version.py
@@ -0,0 +1,3 @@
+"""Version info for project_name."""
+
+__version__ = '0.1.0'
---
+++ project_name/my_module.py
@@ -0,0 +1,10 @@
+"""Main module for the project_name package."""
+
+
+def main():
+    """The main entrypoint for this script used in the setup.py file."""
+    print('Hello World!')
+
+
+if __name__ == '__main__':
+    main()
